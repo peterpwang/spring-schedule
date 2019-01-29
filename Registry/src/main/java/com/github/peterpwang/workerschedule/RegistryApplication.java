3eae3d3f151625d237c80b2af9434d17e9b6b65f
peterpwang <peter.p.wang@outlook.com>
2019-01-29 20:24:23 +1300

    Added registry server (Eureka)

--- Registry/src/main/java/com/github/peterpwang/workerschedule/RegistryApplication.java
+++ Registry/src/main/java/com/github/peterpwang/workerschedule/RegistryApplication.java
@@ -2,17 +2,17 @@ package com.github.peterpwang.workerschedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Spring Boot entry class
  * @author Pei Wang
  */
 @SpringBootApplication
-@EnableEurekaClient 	// Enable eureka client. It inherits from @EnableDiscoveryClient.
-public class WorkerscheduleApplication {
+@EnableEurekaServer	// Enable eureka server
+public class RegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkerscheduleApplication.class, args);
+		SpringApplication.run(RegistryApplication.class, args);
 	}
 }
